e3c0a2928f7d986619730c9810e4d2273be94c61
Vaertrouv <guto.siique@gmail.com>
2021-03-27 16:05:56 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
+  form: FormGroup;
 
-  constructor() {}
+  constructor(public navCtrl: NavController, private formBuilder: FormBuilder) {
+    this.creatreForm();
+    }
 
+    creatreForm() {
+      this.form = this.formBuilder.group({
+        nome: ['', Validators.required],
+        datanascimento: ['', Validators.required],
+        inscricao: ['', Validators.required],
+        zona: ['', Validators.required],
+        secao: ['', Validators.required],
+        cidade: ['', Validators.required],
+        estado: ['', Validators.required],
+        dataemissao: ['', Validators.required],
+        nomepai: ['', Validators.required],
+        nomemae: ['', Validators.required]
+      });
+  }
+
+  salvar() {
+    console.log(this.form.value);
+    this.creatreForm();
+  }
 }
